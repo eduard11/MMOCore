f8effc841e15325d72c5ce0ea9a190b1ff871975
vkbot <info@exalted-world.pp.ua>
2011-08-19 19:39:42 +0300

    Core/Spells: Fix magic suppression talent. (author bleed12) Core/Spells: Fixed Magic Eater Food. (author Lopin)

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -2931,6 +2931,19 @@ void SpellMgr::LoadDbcDataCorrections()
         case 36350: //They Must Burn Bomb Aura (self)
             spellInfo->EffectTriggerSpell[0] = 36325; // They Must Burn Bomb Drop (DND)
             break;
+		case 32645: // Envenom (Rank 1)
+        case 32684: // Envenom (Rank 2)
+        case 57992: // Envenom (Rank 3)
+        case 57993: // Envenom (Rank 4)
+            spellInfo->Dispel = DISPEL_NONE;
+            ++count;
+            break;
+        case 49224: // Magic Suppression Rank 1
+        case 49610: // Magic Suppression Rank 2
+        case 49611: // Magic Suppression Rank 3
+        	spellInfo->procCharges = 0;
+        	++count;
+        	break;
         case 49838: // Stop Time
             spellInfo->AttributesEx3 |= SPELL_ATTR3_NO_INITIAL_AGGRO;
             break;
